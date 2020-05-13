f1ede0e7f55ef177cb1b417aa927efb241784c01
Harry Merritt <harry@merritt.es>
2020-05-13 01:02:37 +0100

    Test repair function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,31 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const defaultItem = {
+    name: "testName",
+    enhancement: 10,
+    durability: 50,
+};
+
+describe("enhancer", () => {
+    it("fn succeed", () => {});
+
+    it("fn fail", () => {});
+
+    it("fn repair", () => {
+        expect(enhancer.repair(defaultItem)).toEqual({
+            ...defaultItem,
+            durability: 100,
+        });
+
+        expect(enhancer.repair({...defaultItem, durability: -10})).toEqual({
+            ...defaultItem,
+            durability: 100,
+        });
+
+        expect(enhancer.repair({...defaultItem, durability: 1000})).toEqual({
+            ...defaultItem,
+            durability: 100,
+        });
+    });
+});
