e670027f2f394b4588ab7392822bb711e8eeff5e
Harry Merritt <harry@merritt.es>
2020-05-13 01:02:25 +0100

    Write initial enhancer functions using = MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,44 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+    succeed,
+    fail,
+    repair,
+    get,
 };
 
 function succeed(item) {
-  return { ...item };
+    if (item.enhancement < 0) item.enhancement = 0; // Stop value going below 0
+    if (item.enhancement < 20) item.enhancement += 1; // Increment if below 20
+    return { ...item };
 }
 
 function fail(item) {
-  return { ...item };
+    // Ammount to decrement durability by
+    let decrementBy = 1;
+
+    //
+    switch (true) {
+        case item.enhancement < 15:
+            decrementBy = 5;
+            break;
+
+        case item.enhancement >= 15:
+            decrementBy = 10;
+            break;
+    }
+
+    // decrement durability
+    item.durability -= decrementBy;
+
+    // Stop value going below 0
+    if (item.durability < 0) item.durability = 0;
+
+    return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+    return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+    return { ...item };
 }
