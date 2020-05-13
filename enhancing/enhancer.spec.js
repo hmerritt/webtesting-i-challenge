aaea03e1db4a9d5fe84aa9a272f8f959f4d6d9cb
Harry Merritt <harry@merritt.es>
2020-05-13 02:00:48 +0100

    Test succeed function

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -8,7 +8,25 @@ const defaultItem = {
 };
 
 describe("enhancer", () => {
-    it("fn succeed", () => {});
+    it("fn succeed", () => {
+        const expectedItem = {
+            ...defaultItem,
+            enhancement: 11,
+        };
+
+        expect(enhancer.succeed(defaultItem)).toEqual(expectedItem);
+
+        expect(enhancer.succeed({ ...defaultItem, enhancement: 19 }))
+            .toEqual({ ...defaultItem, enhancement: 20 });
+
+        expect(enhancer.succeed({ ...defaultItem, enhancement: 20 }))
+            .toEqual({ ...defaultItem, enhancement: 20 });
+
+        expect(enhancer.succeed({ ...defaultItem, enhancement: -10 }).enhancement)
+            .toBeGreaterThanOrEqual(0);
+        expect(enhancer.succeed({ ...defaultItem, enhancement: 200 }).enhancement)
+            .toBeLessThanOrEqual(20);
+    });
 
     it("fn fail", () => {});
 
@@ -20,12 +38,15 @@ describe("enhancer", () => {
 
         expect(enhancer.repair(defaultItem)).toEqual(expectedItem);
 
-        expect(enhancer.repair({ ...defaultItem, durability: -10 })).toEqual(
-            expectedItem
-        );
-    
-        expect(enhancer.repair({ ...defaultItem, durability: 1000 })).toEqual(
-            expectedItem
-        );
+        expect(enhancer.repair({ ...defaultItem, durability: -10 }))
+            .toEqual(expectedItem);
+
+        expect(enhancer.repair({ ...defaultItem, durability: 1000 }))
+            .toEqual(expectedItem);
+
+        expect(enhancer.repair({ ...defaultItem, durability: -10 }).durability)
+            .toBeGreaterThanOrEqual(0);
+        expect(enhancer.repair({ ...defaultItem, durability: 1000 }).durability)
+            .toBeLessThanOrEqual(100);
     });
 });
